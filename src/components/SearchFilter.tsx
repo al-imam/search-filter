54250c8a0930fbbb55695066ef12362b2d722df8
al-imam <mdalimam01828@gmail.com>
2022-11-20 21:26:51 +0600

    create and style searchFilter component
      - use moduler and plain css
      - useFetch cstm hook to fetch data
      - map the data to TR component

---
+++ src/components/SearchFilter.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import useFetch from "../hooks/useFetch";
+import TR from "./TR";
+import classes from "./searchFilter.module.css";
+import { Base } from "../PokemonType";
+
+const calcBase = (base: Base): number => {
+  return Object.values(base).reduce((a: number, v: number): number => a + v, 0);
+};
+
+const SearchFilter: React.FunctionComponent = () => {
+  const [input, setInput] = useState<string>("");
+  const { loading, error, pokemon } = useFetch(
+    "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+  );
+
+  return (
+    <>
+      <input
+        type="text"
+        value={input}
+        onChange={(evt) => setInput(evt.target.value)}
+        placeholder="Search pokemon"
+      />
+      {loading && "Loading...."}
+      {error && "there was an error!!"}
+      {loading || error || (
+        <table className={classes.table}>
+          <thead>
+            <tr>
+              <th className={classes.headerCell}>Name</th>
+              <th className={classes.headerCell}>Type</th>
+              <th className={classes.headerCell}>Base</th>
+            </tr>
+          </thead>
+          <tbody className={classes.tableBody}>
+            {pokemon
+              .filter((p) =>
+                p.name.english.toLowerCase().includes(input.toLowerCase())
+              )
+              .slice(0, 50)
+              .map((p) => (
+                <TR
+                  key={p.id}
+                  name={p.name.english}
+                  type={p.type.join(" ")}
+                  base={calcBase(p.base)}
+                />
+              ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+};
+
+export default SearchFilter;
