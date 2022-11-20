195f15959dc6a82e45d5325d7e2336b7699dcbbe
al-imam <mdalimam01828@gmail.com>
2022-11-20 21:30:01 +0600

    modify App.tsx
      - add heading to app
      - add SearchFilter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import SearchFilter from "./components/SearchFilter";
+
 function App() {
-  return <div>noting here!</div>;
+  return (
+    <div className="holder">
+      <h1>
+        <a href="https://github.com/al-imam">pokémon-table</a>
+      </h1>
+      <SearchFilter />
+    </div>
+  );
 }
 
 export default App;
