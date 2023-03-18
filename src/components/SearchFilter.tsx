8851a3d66772dd3b635503b58805070f94f63e85
al-imam <alimam01828@gmail.com>
2023-03-18 07:37:47 +0600

    style light mood table

--- src/components/SearchFilter.tsx
+++ src/components/SearchFilter.tsx
@@ -35,13 +35,13 @@ const SearchFilter: React.FunctionComponent = () => {
         value={input}
         onChange={(evt) => setInput(evt.target.value)}
         placeholder="Search pokemon"
-        className="p-2 font-cstm caret-current rounded-sm outline-none mb-4 border-none text-center text-gray-700 placeholder:text-slate-300 focus:outline-blue-400 focus:outline-offset-0 outline-4 "
+        className="p-2 font-cstm caret-current rounded-sm outline-none bg-white mb-4 border-none text-center text-gray-700 placeholder:text-slate-300 focus:outline-blue-400 focus:outline-offset-0 outline-4 "
       />
       {loading && "Loading...."}
       {error && "there was an error!!"}
       {loading || error || (
         <table>
-          <thead className=" text-slate-50 bg-indigo-300 bg-opacity-10 ">
+          <thead className=" text-indigo-900 bg-blue-900 bg-opacity-10 dark:text-slate-50 dark:bg-indigo-300 dark:bg-opacity-10 ">
             <tr>
               <th className="p-2 font-cstm text-base uppercase font-light">
                 name
@@ -57,7 +57,7 @@ const SearchFilter: React.FunctionComponent = () => {
               </th>
             </tr>
           </thead>
-          <tbody className="text-slate-300 [&>*:nth-child(even)]:bg-indigo-200 [&>*:nth-child(even)]:bg-opacity-5 ">
+          <tbody className="text-gray-600 dark:text-slate-300 [&>*:nth-child(even)]:bg-indigo-900 [&>*:nth-child(even)]:bg-opacity-5 dark:[&>*:nth-child(even)]:bg-indigo-200 dark:[&>*:nth-child(even)]:bg-opacity-5 ">
             {filter.map((p) => (
               <TR
                 key={p.id}
