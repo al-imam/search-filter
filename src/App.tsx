8b46659a8a65f5f2f8aeb4217e5f5db74a5778b3
al-imam <alimam01828@gmail.com>
2023-03-18 07:00:17 +0600

    change link focus-visible style

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import SearchFilter from "./components/SearchFilter";
 function App() {
   return (
     <div className="flex justify-center flex-col max-w-screen-sm px-4 py-10 mx-auto">
-      <h1 className="text-3xl py-4 text-white">
-        <a href="https://github.com/al-imam">pokémon-table</a>
+      <h1 className="text-3xl py-4 text-white ">
+        <a
+          href="https://github.com/al-imam"
+          className="border-none outline-none rounded focus-visible:outline-blue-400 focus-visible:outline-offset-0 outline-4 "
+        >
+          pokémon-table
+        </a>
       </h1>
       <SearchFilter />
     </div>
