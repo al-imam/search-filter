2905632b9ec7489145943ed3429869abdf5ab63d
al-imam <alimam01828@gmail.com>
2023-03-18 07:24:59 +0600

    change body bg on light theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SearchFilter from "./components/SearchFilter";
 function App() {
   return (
     <div className="flex justify-center flex-col max-w-screen-sm px-4 py-10 mx-auto">
-      <h1 className="text-3xl py-4 text-white ">
+      <h1 className="text-3xl py-4 text-indigo-900 dark:text-white ">
         <a
           href="https://github.com/al-imam"
           className="border-none outline-none rounded focus-visible:outline-blue-400 focus-visible:outline-offset-0 outline-4 "
